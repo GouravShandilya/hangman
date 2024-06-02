604449874c74390c6839a393aec347fd1c6b6ab3
Gourav <shandilya669@gmail.com>
2024-06-02 18:39:04 +0530

    adding new page

---
+++ src/components/Button/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <button :type="type">{{ text }}</button>
+</template>
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: {
+      type: String,
+      default: "button",
+    },
+    type: {
+      type: String,
+      default: "button",
+    },
+  },
+};
+</script>
+<style scoped lang="scss">
+$sel: ".btn";
+#{$sel} {
+  padding: 10px 20px;
+  border-radius: 10px;
+  border: none;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+  &#{$sel}-primary {
+    background-color: $primary-color;
+  }
+  &#{$sel}-secondary {
+    background-color: $primary-color-light-1;
+  }
+  &#{$sel}-danger {
+    background-color: $primary-color-light-2;
+  }
+  &#{$sel}-warning {
+    background-color: $primary-color-light-3;
+  }
+  &#{$sel}-success {
+    background-color: $primary-color-dark-3;
+  }
+  &#{$sel}-info {
+    background-color: $primary-color-light-1;
+  }
+  &:hover {
+    opacity: 0.8;
+    background-color: white;
+    color: $primary-color;
+    border: 1px solid black;
+  }
+}
+</style>
---
+++ src/components/Input/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="main">
+    <label for=""><i class="ri-shining-fill"></i> {{ label }}...</label>
+    <input
+      :type="type"
+      :placeholder="placeholder"
+      :value="modelValue"
+      @input="$emit('input', $event.target.value)"
+    />
+  </div>
+</template>
+<script>
+import Wrapper from "./../Dashboard/index.vue";
+export default {
+  name: "Input",
+  props: {
+    type: {
+      type: String,
+      default: "text",
+    },
+    placeholder: {
+      type: String,
+      default: "",
+    },
+    modelValue: {
+      type: String,
+      default: "",
+    },
+    label: {
+      type: String,
+      default: "",
+    },
+  },
+  components: { Wrapper },
+};
+</script>
+
+<style scoped lang="scss">
+.main {
+  display: flex;
+  flex-direction: column;
+  text-transform: capitalize;
+  label {
+    color: whitesmoke;
+    font-size: 43px;
+  }
+  input {
+    width: 500px;
+    outline: none;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    border-top-right-radius: 30%;
+    border-bottom-right-radius: 30%;
+  }
+}
+</style>
